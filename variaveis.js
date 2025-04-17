22ab5cbc856f2fd3dc827e5e69e73a2e3512316d
Carati32 <brunosilva33357@gmail.com>
2025-04-16 22:19:09 -0300

    declarção de variáveis

---
+++ variaveis.js
@@ -0,0 +1,20 @@
+// declaração de variável com var
+
+/*var nome = "Carati"
+
+var nome = "João"*/
+
+// declaração de variável com let
+
+// let nome = "Carati"
+
+// nome = "João"
+
+// variável declarada com const
+
+const nome = "Carati"
+
+nome = "João"
+
+
+console.log(nome)
