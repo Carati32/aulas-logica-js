264fb07f3dd849e325c910db1dc0130296573749
Carati32 <brunosilva33357@gmail.com>
2025-04-28 22:21:53 -0300

    Aprendemos sobre funções e condicionais

---
+++ condicionais.js
@@ -0,0 +1,25 @@
+// const idade = 15
+// const temCarteira = true
+
+// if(idade >= 18 || temCarteira){
+//     console.log("Pode Dirigir")
+// }
+
+// else{
+//     console.log("Não pode dirigir")
+// }
+
+const notaDeMatematica = 4
+const frequencia = 5.0
+
+if(notaDeMatematica >= 6 && frequencia >= 7.5){
+    console.log("Aprovado")
+}
+
+else if(notaDeMatematica >= 5 && frequencia >= 7.5){
+    console.log("Recuperação")
+}
+
+else{
+    console.log("Reprovado")
+}
---
+++ exercicio03.js
@@ -0,0 +1,32 @@
+//Crie uma função que recebe um parãmetro de media e verifica em um condicional se a nota for maior que 7, o aluno está aprovado,senão,o aluno está reprovado
+
+
+
+const nota = 7
+
+function resultado(nota){
+if(media >= 7){
+    console.log(media +" Aprovado")
+}
+
+else{
+    console.log(media +" Reprovado")
+}
+}
+
+
+// const soma = (x,y,z)=> x+y+z  
+
+// const calculo = (x,y,z)=>{
+//     return soma(x,y,z) / 3
+// }
+
+// const media = calculo(10,8,6)
+
+// if(media >= 7){
+//     console.log(media +" Aprovado")
+// }
+
+// else{
+//     console.log(media +" Reprovado")
+// }
