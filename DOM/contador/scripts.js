7ad5de9303a7cbf98e10a13de739267eeefeda5e
Carati32 <brunosilva33357@gmail.com>
2025-05-07 22:22:03 -0300

    feat: exercicios de js

--- DOM/contador/scripts.js
+++ DOM/contador/scripts.js
@@ -4,7 +4,16 @@ const resultado = document.querySelector("#resultado")
 
 const btnIncrementaValor = document.querySelector("#btnIncrementar")
 
+const btnTirarValor = document.querySelector("#btnTirar")
+
 btnIncrementaValor.addEventListener("click", ()=>{
     contador++
     resultado.textContent = `Contador: ${contador}`
+})
+
+btnTirarValor.addEventListener("click", ()=>{
+    if(contador > 0){
+        contador--
+        resultado.textContent = `Contador: ${contador}`
+    }
 })
